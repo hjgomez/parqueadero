d26949ed04047426304111333c0f3cdd5b37c5da
Jair Gomez <jgp_jair@hotmail.com>
2014-08-25 23:36:05 -0500

    Subiendo mis primeros archivos

---
+++ src/com/cjif/configuracion/Configuracion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.cjif.configuracion;
+
+/**
+ *
+* @author Carlos Arteaga, Francisco Delgado, Ivan Cisneros y Jair Gómez
+ */
+public class Configuracion {
+    
+}
