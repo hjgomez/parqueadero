17ba8fece05fbe1b0abe54931104987ca1fc14e1
Jair Gómez <jgp_jair@hotmail.com>
2014-08-26 11:54:29 -0500

    actualizacion desde Netbeans

---
+++ src/com/umariana/parqueadero/seguridad/Seguridad.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umariana.parqueadero.seguridad;
+
+/**
+ *
+* @author Carlos Arteaga, Francisco Delgado, Ivan Cisneros y Jair Gómez
+ */
+public class Seguridad {
+    
+}
